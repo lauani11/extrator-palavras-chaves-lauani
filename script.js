84417d15e655b35879f39a8caab8d0e013eb4d75
Lauani Vieira <vieira.lauani@escola.pr.gov.br>
2025-10-21 10:11:05 -0300

    21/10

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChaves);
 function mostraPalavrasChaves(){
     const texto = document.querySelector("entrada-de-texto").value;
     const campoResultado= document.querySelector("#resulado-palavrachave")
-    const palavra= texto.split("");
+    const palavra= texto.split(" ");
+
+    campoResultado.textContent= palavra.join(",")
 }
