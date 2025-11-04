2fac5b975aead5a584079765ae4ea3291822808d
Lauani Vieira <vieira.lauani@escola.pr.gov.br>
2025-11-04 09:38:06 -0300

    04/11

--- script.js
+++ script.js
@@ -10,27 +10,32 @@ function mostraPalavrasChave() {
     campoResultado.textContent = palavrasChave.join(", ")
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLoweCase();
+    }
+    palavras = tiraPalavrasRuins(palavras)
+
     const frequencias = contaFrequencias(palavras);
-let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-function ordenaPalavra(p1, p2){
-    return frequencias [p2] - frequencias[p1];
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0, 10);
 }
-return ordenadas.slice(0,10);
-}
-   
 
 
-  function contaFrequencias(palavras){
-  let frequencias = {};
 
-    for (let i of palavras){
+function contaFrequencias(palavras) {
+    let frequencias = {};
+
+    for (let i of palavras) {
         frequencias[i] = 0;
 
-        for (let j of palavras){
+        for (let j of palavras) {
             if (i == j) {
                 frequencias[i]++;
             }
