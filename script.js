06d6937fcd13d6e01c563ddd833e94a9d689f9ee
Lauani Vieira <vieira.lauani@escola.pr.gov.br>
2025-11-11 09:27:25 -0300

    11/11

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins";
+
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
@@ -45,7 +47,7 @@ function contaFrequencias(palavras) {
 
 }
 function tiraPalavrasRuins(palavras) {
-    const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "que"]);
+    
     const palavrasBoas = [];
 
     for (let palavra of palavras) {
